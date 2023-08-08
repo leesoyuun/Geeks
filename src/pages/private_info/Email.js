125907540aacf0630ef0886ca0adae7acea2ad53
leesoyuun <leesoyun4206@naver.com>
2023-08-08 15:04:01 +0900

    frontend: Modify to enter email

--- src/pages/private_info/Email.js
+++ src/pages/private_info/Email.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,  { useState } from "react";
 import TopBar from "../../components/header/Header";
 import Circle from "../../components/number/Number";
 import Button from "../../components/button/Button";
@@ -8,10 +8,11 @@ const Email = () => {
         <div>
             <TopBar></TopBar>
             <div className='main'>
-                <Circle>1</Circle>
+                <Circle></Circle>
                 <div className='email_text'>재학생 인증을 위해<br/>학교 이메일 주소를 입력해 주세요.</div>
-                <div className='input_email'>
-                    <input type='number'/>
+                <div className='input_emails'>
+                    <input className='input_email' type='number'/>
+                    <div className='univ'>@sangmyung.kr</div>
                 </div>
                 <div className='content'>
                     <Button content='인증 메일 받기'></Button>
