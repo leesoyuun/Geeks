35f92747539d6388f617a23e3598f294e6ddbfbd
leesoyuun <leesoyun4206@naver.com>
2023-08-08 17:39:41 +0900

    frontend: Add nickname length count function

--- src/pages/private_info/Nickname.js
+++ src/pages/private_info/Nickname.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TopBar from "../../components/header/Header";
 import NumberTitle from "../../components/number/Number";
 import Button from "../../components/button/Button";
 import './Nickname.css'
 
 const Nickname = () => {
-    return(
+    const [NumCount, setNumCount] = useState(0);
+
+    const handleChange = (event) => {
+        const newValue = event.target.value;
+
+        if (newValue.length <= 8) {
+            setNumCount(newValue.length);
+        }
+    };
+    return (
         <div className='main'>
             <TopBar></TopBar>
             <NumberTitle title={`회원님을 표현한\n 닉네임을 알려주세요!`}></NumberTitle>
-            <input className='input_nick' type='text'/>
+            <input className='input_nick' type='text' onChange={handleChange}/>
+            <div className='nick_cnt'>{NumCount}/8</div>
             <div className='content'>
                 <Button content='다음'></Button>
             </div>
