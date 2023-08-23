869cc294151656a1bb5ce38555911bc06a8f229b
leesoyuun <leesoyun4206@naver.com>
2023-08-23 15:59:36 +0900

    Show the profile image on the condition I selected

--- src/components/conditionBox/ComditionSub.js
+++ src/components/conditionBox/ComditionSub.js
@@ -1,4 +1,5 @@
 import React from "react";
+import people from "../../static/img/people.png"
 import "./ConditionSub.css";
 
 const ConditionSub = (props) =>{
@@ -6,6 +7,9 @@ const ConditionSub = (props) =>{
     return(
         <div className='condition-sub-box'>
             <div className='condition_name'>{props.condition}</div>
+            <div className='circle-container'>
+                {props.condition === '잠버릇 있어요' && <img src={people} className='circle_photo'/>}
+            </div>
         </div>
 
     )
--- src/components/conditionBox/ConditionBox.js
+++ src/components/conditionBox/ConditionBox.js
@@ -8,11 +8,52 @@ const ConditionBox = (props) =>{
         <div className='condition-boxes'>
             <div className='condition_name'>{props.ConditionName}</div>
             <div className='contions'>
+               {props.ConditionName === '숙면' && (
+                <>
                 <ConditionSub condition={'잠버릇 있어요'}/>
                 <ConditionSub condition={'잠버릇 없어요'}/>
                 <ConditionSub condition={'가끔 있어요'}/>
                 <ConditionSub condition={'귀 밝아요'}/>
                 <ConditionSub condition={'귀 어두워요'}/>
+                </>
+               )}
+               {props.ConditionName === '취침' && (
+                <>
+                <ConditionSub condition={'일찍 자요'}/>
+                <ConditionSub condition={'늦게 자요'}/>
+                <ConditionSub condition={'때마다 달라요'}/>
+                </>                
+               )}
+               {props.ConditionName === '기상' && (
+                <>
+                <ConditionSub condition={'일찍 일어나요'}/>
+                <ConditionSub condition={'늦게 일어나요'}/>
+                <ConditionSub condition={'때마다 달라요'}/>
+                </>                
+               )}
+               {props.ConditionName === '외출' && (
+                <>
+                <ConditionSub condition={'집순이에요'}/>
+                <ConditionSub condition={'밖순이에요'}/>
+                <ConditionSub condition={'약속이 있으면 나가요'}/>
+                <ConditionSub condition={'본가 자주 가요'}/>
+                <ConditionSub condition={'본가 잘 안가요'}/>
+                <ConditionSub condition={'학교에 오래 있어요'}/>
+                </>                
+               )}
+               {props.ConditionName === '청소' && (
+                <>
+                <ConditionSub condition={'자주 청소해요'}/>
+                <ConditionSub condition={'더러워지면 청소해요'}/>
+                <ConditionSub condition={'상대에게 맞춰요'}/>
+                </>                
+               )}
+               {props.ConditionName === '성향' && (
+                <>
+                <ConditionSub condition={'혼자 조용히 지내요'}/>
+                <ConditionSub condition={'룸메와 함께 놀고 싶어요'}/>
+                </>                
+               )}
             </div>
         </div>
 
