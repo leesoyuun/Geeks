1a5b2c3ad27a2b6f626984798e5dd9d9f2987dd4
leesoyuun <leesoyun4206@naver.com>
2023-08-24 22:57:09 +0900

    Button color changes in gender page

--- src/pages/private_info/Gender.js
+++ src/pages/private_info/Gender.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import TopBar from "../../components/header/Header";
 import NumberTitle from "../../components/number/Number";
 import Button from "../../components/button/Button";
@@ -8,10 +8,15 @@ import { UserContext } from "./Register";
 
 const Gender = () => {
   const { setStep } = useContext(UserContext);
+  const [buttonColor,setButtonColor] = useState('gray_btn_color')
 
   const handleClick = () => {
     setStep(7);
   };
+
+  const changebtn = () =>{
+    setButtonColor('yellow_btn_color')
+  }
 
   return (
     <div className="screen">
@@ -19,13 +24,13 @@ const Gender = () => {
         <div className="screen_content">
           <TopBar></TopBar>
           <NumberTitle title={`성별을 알려주세요`} flag={3}></NumberTitle>
-          <div className="gender_select">
+          <div className="gender_select" onClick={changebtn}>
             <SelectBtn gender={`남자`}></SelectBtn>
             <SelectBtn gender={`여자`}></SelectBtn>
           </div>
         </div>
         <div className="private_bottom" onClick={handleClick}>
-          <Button content={`다음`}></Button>
+          <Button content={`다음`} name={buttonColor}></Button>
         </div>
       </div>
     </div>
