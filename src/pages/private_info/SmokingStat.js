b28b0b9014d041b68b39c164c8f61ebfcaeec35a
leesoyuun <leesoyun4206@naver.com>
2023-08-24 23:10:52 +0900

    Button color changes in smokingstate  page

--- src/pages/private_info/SmokingStat.js
+++ src/pages/private_info/SmokingStat.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import TopBar from "../../components/header/Header";
 import NumberTitle from "../../components/number/Number";
 import Button from "../../components/button/Button";
@@ -8,10 +8,15 @@ import { UserContext } from "./Register";
 
 const SmokingStat = () => {
   const { setStep } = useContext(UserContext);
+  const [buttonColor, setButtonColor] = useState('gray_btn_color')
 
   const handleClick = () => {
     setStep(9);
   };
+
+  const changebtn = () => {
+    setButtonColor('yellow_btn_color')
+  }
 
   return (
     <div className="screen">
@@ -19,13 +24,13 @@ const SmokingStat = () => {
         <div className="screen_content">
           <TopBar></TopBar>
           <NumberTitle title={`흡연자 이신가요?`} flag={3}></NumberTitle>
-          <div className="gender_select">
+          <div className="gender_select" onClick={changebtn}>
             <SelectBtnSmoke smokestat={`네`}></SelectBtnSmoke>
             <SelectBtnSmoke smokestat={`아니요`}></SelectBtnSmoke>
           </div>
         </div>
         <div className="private_bottom" onClick={handleClick}>
-          <Button content={`다음`}></Button>
+          <Button content={`다음`} name={buttonColor}></Button>
         </div>
       </div>
     </div>
