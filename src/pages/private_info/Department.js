45513edde74aa369b21b991ce50b94df7de94a9e
leesoyuun <leesoyun4206@naver.com>
2023-08-24 22:50:40 +0900

    Button color changes in department page

--- src/pages/private_info/Department.js
+++ src/pages/private_info/Department.js
@@ -1,4 +1,4 @@
-import React, { useRef, useContext } from "react";
+import React, { useRef, useContext, useState } from "react";
 import { UserContext } from "./Register";
 import TopBar from "../../components/header/Header";
 import NumberTitle from "../../components/number/Number";
@@ -6,12 +6,20 @@ import Button from "../../components/button/Button";
 
 const Department = () => {
   const majorRef = useRef("");
+  const student_ID = useRef("");
   const { setStep, setMajor } = useContext(UserContext);
+  const [buttonColor, setButtonColor] = useState('gray_btn_color');
 
   const handleClick = () => {
     setMajor(majorRef.current.value);
     setStep(6);
   };
+
+  const inputValue = (e) =>{
+    const major_value = majorRef.current.value;
+    const student_id = student_ID.current.value;
+    (major_value.length > 1 && student_id.length > 1) ? setButtonColor('yellow_btn_color') : setButtonColor('gray_btn_color')
+  }
 
   return (
     <div className="screen">
@@ -23,16 +31,16 @@ const Department = () => {
             flag={3}
           ></NumberTitle>
           <div className="input_infos">
-            <input className="input_info_de" type="text" ref={majorRef} />
+            <input className="input_info_de" type="text" ref={majorRef} onChange={inputValue}/>
             <div className="univ">학과/전공</div>
           </div>
           <div className="input_infos_ID">
-            <input className="input_info_ID" type="number" />
+            <input className="input_info_ID" type="number" ref={student_ID} onChange={inputValue}/>
             <div className="univ">학번</div>
           </div>
         </div>
         <div className="private_bottom" onClick={handleClick}>
-          <Button content="다음"></Button>
+          <Button content="다음" name={buttonColor}></Button>
         </div>
       </div>
     </div>
